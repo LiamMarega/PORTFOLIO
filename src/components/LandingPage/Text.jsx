1b9ca31dc87f54de37f7949e6057d28cda699874
LiamMarega <liammarega85@gmail.com>
2021-12-09 22:02:24 -0300

    cambios query

--- src/components/LandingPage/Text.jsx
+++ src/components/LandingPage/Text.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import s from "./Text.module.css"
+import React from "react";
+import s from "./Text.module.css";
+import foto from "./flecha.png";
 
 function Text() {
-    return (
-      <div className={s.text}>
-        <h1>Liam Marega</h1>
-        <br />
-        &mdash; Full Stack Developer &mdash;
-       
-          <br />
-      
+  return (
+    <div className={s.text}>
+      <h1>Liam Marega</h1>
+      <br />
+      &mdash; Full Stack Developer &mdash;
+      <br />
+      <div className={s.imagen}>
+        <img src={foto} alt="flecha" />
       </div>
-    );
+    </div>
+  );
 }
 
 export default Text;
