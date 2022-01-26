4d4c9588f620d06f4e2b2d76f64151fd85665f04
Liam Marega <liammarega85@gmail.com>
2022-01-26 16:44:08 -0300

    Arreglos Resoluciones

--- src/components/FormSubmit/FormSubmit.jsx
+++ src/components/FormSubmit/FormSubmit.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ContactForm from "./ContactForm";
 import s from "./FormSubmit.module.css"
-
 function FormSubmit() {
     return (
         <div className={s.FormSubmit}>
+       
             <ContactForm />
         </div>
     )
